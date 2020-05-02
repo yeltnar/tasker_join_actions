50cabbd4dfa488422e531f0622312edef6061ec3
andcbrant <andcbrant@penguin>
2020-05-02 16:42:32 -0500

    first

---
+++ parse_file.js
@@ -0,0 +1,28 @@
+function readFile(){
+    const file_contents = {
+        notifications:{
+            1:{},
+            2:{},
+            3:{},
+        }
+    };
+    Object.keys(file_contents.notifications).forEach((key,i,arr)=>{
+        const cur = file_contents.notifications[key];
+        cur.title = "";
+        cur.text = "";
+        cur.persistant = "";
+        cur.id = key;
+    });
+    return JSON.stringify(file_contents);    
+};
+
+
+(()=>{
+
+
+    const file_text = readFile();
+    const file = JSON.parse(file_text);
+
+    console.log(file)
+
+})()
